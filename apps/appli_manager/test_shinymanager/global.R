72df751e220108ebd058fce69d110b3e3ad061a6
RozennGZ <rozie229@hotmail.com>
2020-11-03 15:37:08 +0100

    add mysql database

--- apps/appli_manager/test_shinymanager/global.R
+++ apps/appli_manager/test_shinymanager/global.R
@@ -1,13 +1,23 @@
-credentials <- data.frame(
-  user = c("shiny", "shinymanager"), # mandatory
-  password = c("azerty", "12345"), # mandatory
-  start = c("2019-04-15"), # optinal (all others)
-  expire = c(NA, "2019-12-31"),
-  admin = c(FALSE, TRUE),
-  comment = "Simple and secure authentification mechanism 
-  for single ‘Shiny’ applications.",
-  stringsAsFactors = FALSE
-)
+# credentials <- data.frame(
+#   user = c("shiny", "shinymanager"), # mandatory
+#   password = c("azerty", "12345"), # mandatory
+#   start = c("2019-04-15"), # optinal (all others)
+#   expire = c(NA, "2019-12-31"),
+#   admin = c(FALSE, TRUE),
+#   comment = "Simple and secure authentification mechanism
+#   for single ‘Shiny’ applications.",
+#   stringsAsFactors = FALSE
+# )
 
 library(shiny)
 library(shinymanager)
+
+library(keyring)
+ #key_set("R-shinymanager-key", "obiwankenobi")
+
+shinymanager::create_db(
+  credentials_data = credentials,
+  sqlite_path = "database.sqlite", # will be created
+  passphrase = key_get("R-shinymanager-key", "obiwankenobi")
+  # passphrase = "passphrase_wihtout_keyring"
+)
--- apps/appli_manager/test_shinymanager/ui.R
+++ apps/appli_manager/test_shinymanager/ui.R
@@ -1,4 +1,5 @@
 secure_app(fluidPage(
   tags$h2("My secure application"),
   verbatimTextOutput("auth_output")
+  , enable_admin = TRUE
 ))
