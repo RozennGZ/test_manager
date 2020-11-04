65f0d696133630d6ff9c6109fa12c251b2788091
RozennGZ <rozie229@hotmail.com>
2020-11-04 09:35:58 +0100

    creation of the key

--- apps/appli_manager/test_shinymanager/global.R
+++ apps/appli_manager/test_shinymanager/global.R
@@ -1,7 +1,7 @@
 # credentials <- data.frame(
 #   user = c("shiny", "shinymanager"), # mandatory
 #   password = c("azerty", "12345"), # mandatory
-#   start = c("2019-04-15"), # optinal (all others)
+#   start = c("2019-04-15",NA), # optinal (all others)
 #   expire = c(NA, "2022-12-31"),
 #   admin = c(FALSE, TRUE),
 #   comment = "Simple and secure authentification mechanism
@@ -12,10 +12,10 @@
 library(shiny)
 library(shinymanager)
 
-# library(keyring)
+library(keyring)
 # keyring_create("R-shinymanager-key")
-# key_set_with_value("shinymanager-key", "obiwankenobi",password="Tsuki",keyring = "R-shinymanager-key")
-
+# key_set_with_value(service="shinymanager-key",password="Tsuki", username="obiwankenobi",keyring = "R-shinymanager-key")
+# 
 # shinymanager::create_db(
 #   credentials_data = credentials,
 #   sqlite_path = "apps/appli_manager/test_shinymanager/database.sqlite", # will be created
--- apps/appli_manager/test_shinymanager/server.R
+++ apps/appli_manager/test_shinymanager/server.R
@@ -1,10 +1,10 @@
 function(input, output, session) {
   # check_credentials directly on sqlite db
   res_auth <- secure_server(
+
     check_credentials = check_credentials(
       "database.sqlite",
-      passphrase = key_get("shinymanager-key", "obiwankenobi",keyring="R-shinymanager-key")
-      # passphrase = "passphrase_wihtout_keyring"
+      passphrase = keyring::key_get(service="shinymanager-key", username="obiwankenobi",keyring="R-shinymanager-key")
     )
   )
   
